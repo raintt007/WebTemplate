c285e69c12204e4e2b0dd53d71420caa6096d730
tt <695768421@qq.com>
2020-12-04 01:59:40 +0800

    rua!

--- src/pages/signInSignUpForm/js/signInSignUpForm.js
+++ src/pages/signInSignUpForm/js/signInSignUpForm.js
@@ -2,3 +2,12 @@
  * @since 2020/12/03 19:10:29
  */
 
+const signInBtn = document.querySelector('.sign-in-btn');
+const signUpBtn = document.querySelector('.sign-up-btn');
+const container = document.querySelector('.signInSignUpForm-wrapper');
+signUpBtn.addEventListener('click', () =>{
+    container.classList.add('sign-up-mode')
+})
+signInBtn.addEventListener('click', () =>{
+    container.classList.remove('sign-up-mode')
+})
