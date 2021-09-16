4fca41629ba2563b53e0f0976ba218d771593887
tt <695768421@qq.com>
2021-09-16 23:49:40 +0800

    no message

--- src/pages/helloWorld/js/helloWorld.js
+++ src/pages/helloWorld/js/helloWorld.js
@@ -1,29 +1,27 @@
-import createElement from './element';
-import diff from './diff';
-import patch from './patch';
 
-// 1. 构建虚拟DOM
-var tree = createElement('div', {'id': 'container'}, [
-    createElement('h1', {style: 'color: blue'}, ['simple virtal dom']),
-    createElement('p', ['Hello, virtual-dom']),
-    createElement('ul', [createElement('li')])
-])
+  function throttle(fun, delay) {
+    let last, deferTimer
+    return function (args) {
+        let that = this
+        let _args = arguments
+        let now = +new Date()
+        if (last && now < last + delay) {
+            clearTimeout(deferTimer)
+            deferTimer = setTimeout(function () {
+                last = now
+                fun.apply(that, _args)
+            }, delay)
+        }else {
+            last = now
+            fun.apply(that,_args)
+        }
+    }
+}
 
-// 2. 通过虚拟DOM构建真正的DOM
-var root = tree.render()
-document.body.appendChild(root)
+let throttleAjax = throttle(() => {console.log(123)}, 1000)
 
-// 3. 生成新的虚拟DOM
-var newTree = createElement('div', {'id': 'container'}, [
-    createElement('h1', {style: 'color: red'}, ['simple virtal dom']),
-    createElement('p', ['Hello, virtual-dom']),
-    createElement('ul', [createElement('li', {}, ['item']), createElement('li')])
-])
+let inputc = document.querySelector('.throttle')
+inputc.addEventListener('keyup', function(e) {
+    throttleAjax(e.target.value)
+})
 
-setTimeout(() => {
-    // 4. 比较两棵虚拟DOM树的不同
-var patches = diff(tree, newTree)
-
-// 5. 在真正的DOM元素上应用变更
-patch(root, patches)
-}, 2000)
--- src/pages/virtal-dom-toy/js/virtal-dom-toy.js
+++ src/pages/virtal-dom-toy/js/virtal-dom-toy.js
@@ -1,29 +1,29 @@
-import createElement from './element';
-import diff from './diff';
-import patch from './patch';
+// import createElement from './element';
+// import diff from './diff';
+// import patch from './patch';
 
-// 1. 构建虚拟DOM
-var tree = createElement('div', {'id': 'container'}, [
-    createElement('h1', {style: 'color: blue'}, ['simple virtal dom']),
-    createElement('p', ['Hello, virtual-dom']),
-    createElement('ul', [createElement('li')])
-])
+// // 1. 构建虚拟DOM
+// var tree = createElement('div', {'id': 'container'}, [
+//     createElement('h1', {style: 'color: blue'}, ['simple virtal dom']),
+//     createElement('p', ['Hello, virtual-dom']),
+//     createElement('ul', [createElement('li')])
+// ])
 
-// 2. 通过虚拟DOM构建真正的DOM
-var root = tree.render()
-document.body.appendChild(root)
+// // 2. 通过虚拟DOM构建真正的DOM
+// var root = tree.render()
+// document.body.appendChild(root)
 
-// 3. 生成新的虚拟DOM
-var newTree = createElement('div', {'id': 'container'}, [
-    createElement('h1', {style: 'color: red'}, ['simple virtal dom']),
-    createElement('p', ['Hello, virtual-dom']),
-    createElement('ul', [createElement('li', {}, ['item']), createElement('li')])
-])
+// // 3. 生成新的虚拟DOM
+// var newTree = createElement('div', {'id': 'container'}, [
+//     createElement('h1', {style: 'color: red'}, ['simple virtal dom']),
+//     createElement('p', ['Hello, virtual-dom']),
+//     createElement('ul', [createElement('li', {}, ['item']), createElement('li')])
+// ])
 
-setTimeout(() => {
-    // 4. 比较两棵虚拟DOM树的不同
-var patches = diff(tree, newTree)
+// setTimeout(() => {
+//     // 4. 比较两棵虚拟DOM树的不同
+// var patches = diff(tree, newTree)
 
-// 5. 在真正的DOM元素上应用变更
-patch(root, patches)
-}, 2000)
+// // 5. 在真正的DOM元素上应用变更
+// patch(root, patches)
+// }, 2000)
